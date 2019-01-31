b71226a2240453abb04d75cf3542d3ad3cbd41b4
Max Niebergall <max@maxniebergall.com>
2019-01-31 13:25:49 -0500

    Update Recommend.py

--- Recommend.py
+++ Recommend.py
@@ -1,7 +1,9 @@
 import itertools
 
-browsing = open("F:/Programming/CP421/browsing.txt")
-
+browsing = open("./browsing.txt")
+#items_fv = open("./items.txt", "w")
+pairs_fv = open("./pairs.txt", "w")
+trips_fv = open("./triples.txt", "w")
 
 items = dict()
 
@@ -18,10 +20,14 @@ while len(line) != 0:
 #get only items with at elast 100 occurances
 #TODO Change this back to 100
 #items_list = [k for k,v in items.items() if int(v) >= 100]
+
 for k,v in list(items.items()):
     if int(v) <100:
-
         del items[k]
+
+items_list = list(items.items())
+
+items_list.sort()
 
 item_pairs_dict = dict()
 
@@ -36,35 +42,101 @@ while len(line) != 0:
         for j in range(i):
             if ( i!= j and line_items[i] in items and line_items[j] in items):
                 item_pairs_dict[line_items[i] + " " + line_items[j]] = item_pairs_dict.get(line_items[i] + " " + line_items[j], 0) + 1
-
     line = browsing.readline()
 
-item_trips_dict = dict()
 
 for k,v in list(item_pairs_dict.items()):
-    if(v > 100):
-        print(k,v)
+    if int(v) <100:
+        del item_pairs_dict[k]
 
 
+item_pairs_list = list(item_pairs_dict.items())
+
+item_pairs_list.sort()
+
+item_pairs_list_sorted_by_conf = []
+
+for k,v in item_pairs_list:
+    conf = items.get(k.split(" ")[0])
+    item_pairs_list_sorted_by_conf.append((v/conf, k))
+    
+item_pairs_list_sorted_by_conf.sort()
+
+for k,v in item_pairs_list_sorted_by_conf:
+	pairs_fv.write(str(k) + " " + str(v) + "\n")
+
+#------------------------------------------------------------------- triples
+item_trips_dict = dict()
+
+for i in range(len(item_pairs_list)):
+	for j in range(i):
+		if(i != j):
+			first = item_pairs_list[i][0].split(" ")
+			second = item_pairs_list[j][0].split(" ")
+			if first[0] == second[0]:
+				item_trips_dict[first[0] + " " + first[1]+ " " + second[1]] = 0
+
+
+
+
+#---------------------- read in triples
 browsing.seek(0)
 line = browsing.readline()
-
 
 while len(line) != 0:
     line_items = line.strip().split(" ")
 
     for i in range(len(line_items)):
-        for j in range(i):
-            for k in range(j):
-                if ( (i!=j and i!=k and j!=k) ):
-                    item_trips_dict[line_items[i] + " " + line_items[j]+ " " + line_items[k]] = item_pairs_dict.get(line_items[i] + " " + line_items[j]+ " " + line_items[k], 0) + 1
+        for j in range(len(line_items)):
+            for k in range(len(line_items)):
+                if ( (i!=j and i!=k and j!=k)):
+                    combo = (line_items[i] + " " + line_items[j]+ " " + line_items[k])
+                    if(combo in item_trips_dict):
+                        item_trips_dict[combo] = item_trips_dict.get(combo) + 1
 
     line = browsing.readline()
 
+item_trips_list = list(item_trips_dict.items())
+item_trips_list.sort()
 
-item_trips_dict[line_items[i] + " " + line_items[j]]
-item_trips_dict[line_items[i] + " " + line_items[k]]
-item_trips_dict[line_items[j]+ " " + line_items[k]]
-item_trips_dict[line_items[i] + " " + line_items[j]+ " " + line_items[k]]
+
+for k,v in list(item_trips_dict.items()):
+    if int(v) <100:
+        del item_trips_dict[k]
+    
+    
+for k,v in list(item_trips_dict.items()):
+    items_local = k.split(" ")
+    conf = item_pairs_dict.get(items_local[0] + " " + items_local[1])
+    item_pairs_list_sorted_by_conf.append((v/conf, k))
+    
+item_pairs_list_sorted_by_conf.sort()    
+    
+for k,v in item_pairs_list_sorted_by_conf:
+    trips_fv.write(str(k) + " " + str(v) + "\n")
+
+
+browsing.close()
+#items_fv.close()
+pairs_fv.close()
+trips_fv.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
